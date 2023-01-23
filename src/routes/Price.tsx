6396b8c85d5e90299074c0a30079de517cbff427
borat <boram8655@gmail.com>
2023-01-23 17:10:56 -0300

    type error

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,4 +1,26 @@
-function Price() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface PriceProps {
+  coinId: string;
+}
+
+interface IHistorial {
+  time_open: string;
+  time_close: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+function Price({ coinId }: PriceProps) {
+  const { isLoading, data } = useQuery<IHistorial[]>(["ticker", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  console.log(data);
   return <h1>Price</h1>;
 }
 
